a20b2edf1e8b6daced6536b384a0b4b1b1cc2e4c
dongjian <dongjian@dongjiandeMacBook-Pro.local>
2026-04-24 09:44:01 +0800

    fix: 开启静态模式

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,7 @@ import decapCmsOauth from 'astro-decap-cms-oauth';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [decapCmsOauth()]
+  integrations: [decapCmsOauth()],
+  // 👇 这是关键：强制设置为纯静态模式
+  output: "static"
 });
