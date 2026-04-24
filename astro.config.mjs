940b68cfb5ab7f2f79c99b2c5db47874c305dd26
dongjian <dongjian@dongjiandeMacBook-Pro.local>
2026-04-24 09:54:07 +0800

    remove decap cms

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,7 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
-import decapCmsOauth from 'astro-decap-cms-oauth';
-
 // https://astro.build/config
 export default defineConfig({
-  integrations: [decapCmsOauth()],
-  // 👇 这是关键：强制设置为纯静态模式
   output: "static"
 });
